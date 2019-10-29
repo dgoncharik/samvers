84097f016cd32fcfdff6f98bdad095e2243ff5de
Гончарик Денис <dgoncharik27@gmail.com>
2019-10-29 18:04:59 +0300

    сверстал

--- source/js/main.js
+++ source/js/main.js
@@ -1,4 +1,8 @@
 let TABLET_WIDTH = 768;
+let headerBurger = document.querySelector('.header__burger');
+let mainMenu = document.querySelector('.header__main-navigation');
+let mainMenuBurger = mainMenu.querySelector('.main-navigation__burger');
+let MENU_OPENED = 'main-navigation--opened';
 
 function slickOn(jqElem) {
   jqElem.slick({
@@ -10,6 +14,39 @@ function slickOn(jqElem) {
   });
 }
 
+function menuIsOpened() {
+  return mainMenu.classList.contains(MENU_OPENED);
+}
+
+function showMenu() {
+  mainMenu.classList.add(MENU_OPENED);
+}
+
+function hideMenu() {
+  mainMenu.classList.remove(MENU_OPENED);
+}
+
 if ($(window).width() < TABLET_WIDTH) {
   slickOn( $('.slider') );
 }
+
+headerBurger.addEventListener('click', function(evt) {
+  evt.preventDefault();
+  evt.stopPropagation();
+  showMenu();
+})
+
+mainMenuBurger.addEventListener('click', function(evt) {
+  evt.preventDefault();
+  evt.stopPropagation();
+  hideMenu();
+})
+
+$(document).click(function(evt) {
+   var elem = $('.header__main-navigation');
+   console.log(elem);
+   console.log(evt.target);
+   if(evt.target != elem[0] && !elem.has(evt.target).length) {
+     hideMenu();
+   } 
+});
